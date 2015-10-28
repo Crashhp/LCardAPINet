df8cb435f5a50291784bdbe72dee50e51ad218c3
Fadeev Vadim <xboxfadeev@gmail.com>
2015-10-29 00:42:10 +0600

    issue-1 gui to read data from E20-10

---
+++ LCardAPI/LCard.E2010GUI/View/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MahApps.Metro.Controls;
+
+namespace LCard.E2010GUI.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
